39ec2e435abbbdd9bc5c9a74a1458745dcbe275d
chris-schmitz <schmitz.chris@gmail.com>
2020-04-23 09:48:22 -0500

    removes software serial stuff

--- circuit-playground-motion-sensor/circuit-playground-motion-sensor.ino
+++ circuit-playground-motion-sensor/circuit-playground-motion-sensor.ino
@@ -1,18 +1,11 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial sSerial(10, 11);
-
 void setup()
 {
-    Serial1.begin(9600);
-    sSerial.begin(9600);
+    Serial.begin(9600);
 }
 void loop()
 {
-    Serial1.println(0);
+    Serial.println(0);
     delay(1000);
-    Serial1.println(1);
-    delay(1000);
-    sSerial.println(1);
+    Serial.println(1);
     delay(1000);
 }
